6e6c0699c1dcffde8f4b97cbcdcdc86d00a81516
Thomas Castiglione <castiglione@mac.com>
2017-09-25 00:38:23 +0800

    annoyingly unminimal web test

---
+++ Dispel.Web/Pages/Download.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dispel.Web.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
---
+++ Dispel.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dispel.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
