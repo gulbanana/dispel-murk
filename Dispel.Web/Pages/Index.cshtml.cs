df8690ab4e5fcddc222ddf9a92694a33b07cc19a
Thomas Castiglione <castiglione@mac.com>
2017-09-25 19:04:02 +0800

    send the html result directly back to the client

--- Dispel.Web/Pages/Index.cshtml.cs
+++ Dispel.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Dispel.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        [Required, Display(Name = "Log File")]
+        [BindProperty, Required, Display(Name = "Log File")]
         public IFormFile LogFile { get; set; }
+
+        public async Task<ObjectResult> OnPostAsync()
+        {
+            using (var file = LogFile.OpenReadStream())
+            {
+                var stream = new MemoryStream();
+                await Engine.ConvertAsync(file, Response.Body, OutputFormat.HTML);
+                return new ObjectResult(stream);
+            }
+        }
     }
 }
