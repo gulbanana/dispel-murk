1e0467d5d205d48a4ce88af3f4479cab0c6aef85
Thomas Castiglione <castiglione@mac.com>
2019-11-15 23:15:23 +0800

    index page for web output

--- Dispel/AST/Session.cs
+++ Dispel/AST/Session.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Dispel.AST
@@ -7,11 +9,31 @@ namespace Dispel.AST
     {
         public readonly string Ident;
         public readonly Line[] Body;
+        public readonly string StartTime;
+        public readonly string EndTime;
 
         public Session(string ident, string startTime, string endTime, IEnumerable<Line> body)
         {
             Ident = ident;
             Body = body.ToArray();
+
+            if (DateTime.TryParseExact(startTime, "ddd MMM dd HH:mm:ss yyyy", null, DateTimeStyles.None, out var startDate))
+            {
+                StartTime = startDate.ToShortDateString();
+            }
+            else
+            {
+                StartTime = startTime;
+            }
+
+            if (DateTime.TryParseExact(endTime, "ddd MMM dd HH:mm:ss yyyy", null, DateTimeStyles.None, out var endDate))
+            {
+                EndTime = endDate.ToShortDateString();
+            }
+            else
+            {
+                EndTime = endTime;
+            }
         }
     }
 }
--- Dispel/WebGenerator.cs
+++ Dispel/WebGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using Dispel.AST;
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,61 +10,31 @@ namespace Dispel
     static class WebGenerator
     {
         private const string prefix = @"<!DOCTYPE html>
-<head>
-    <meta charset=""utf-8"">
-    <title>mIRC Logfile</title>
-    <style>
-    body {
-        font-family: -apple-system, BlinkMacSystemFont, ""Segoe UI"", Roboto, Oxygen-Sans, Ubuntu, Cantarell, ""Helvetica Neue"", sans-serif;
-        color: rgb(15, 15, 15);
-    }
-    p.line {
-        margin: 0;
-        padding: 0;
-    }
-    span.user {
-        font-weight: bold;
-    }
-    span.timestamp {
-        color: rgb(128, 128, 128);
-    }
-    span.b { font-weight: bold; }
-    span.u { text-decoration: underline; }
-    span.i { font-style: italic; }
-    span.c0 { color: rgb(255,255,255); }
-    span.c1 { color: rgb(0,0,0); }
-    span.c2 { color: rgb(0,0,127); }
-    span.c3 { color: rgb(0,147,0); }
-    span.c4 { color: rgb(255,0,0); }
-    span.c5 { color: rgb(136,72,72); }
-    span.c6 { color: rgb(156,0,156); }
-    span.c7 { color: rgb(252,127,0); }
-    span.c8 { color: rgb(255,255,0); }
-    span.c9 { color: rgb(0,252,0); }
-    span.c10 { color: rgb(0,147,147); }
-    span.c11 { color: rgb(0,255,255); }
-    span.c12 { color: rgb(0,0,252); }
-    span.c13 { color: rgb(255,0,255); }
-    span.c14 { color: rgb(127,127,127); }
-    span.c15 { color: rgb(210,210,210); }
-    </style>
-</head>
-<body>";
+<meta charset=""utf-8"">
+<title>mIRC Logfile</title>
+";
 
-        private const string suffix = @"</body>
-</html>";
+        private const string stylesheet = @"<link href=""style.css"" rel=""stylesheet"">
+";
 
         public static OutputFile[] FormatSite(Log log)
         {
-            return log.Sessions.Select(Format).ToArray();
+            return log.Sessions.Select(Format).Append(new OutputFile(
+                "style.css",
+                ReadStylesheet()
+            )).Append(new OutputFile(
+                "index.html",
+                CreateIndex(log)
+            )).ToArray();
         }
 
         public static OutputFile Format(Session session, int index)
         {
             var bodyContent = string.Join("", session.Body.Select(Format));
+
             return new OutputFile(
                 $"{session.Ident}-{index}.html",
-                prefix + bodyContent
+                prefix + stylesheet + bodyContent
             );
         }
 
@@ -71,10 +42,15 @@ namespace Dispel
         {
             var firstIdent = log.Sessions.First().Ident;
             var bodyContent = string.Join("", log.Sessions.SelectMany(s => s.Body).Select(Format));
+            var stylesheet = @"<style type=""text/css"">
+" + ReadStylesheet() + @"
+</style>
+";
+
             return new[] {
                 new OutputFile(
                     $"{firstIdent}.html",
-                    prefix + bodyContent
+                    prefix + stylesheet + bodyContent
                 )
             };
         }
@@ -145,6 +121,38 @@ namespace Dispel
                 if (textClass != "") builder.Append("</span>");
             }
             return builder.ToString();
+        }
+
+        private static string ReadStylesheet()
+        {
+            return new StreamReader(typeof(WebGenerator).Assembly.GetManifestResourceStream("Dispel.style.css")).ReadToEnd();
+        }
+
+        private static string CreateIndex(Log log)
+        {
+            var title = log.Sessions.First().Ident;
+            var links = log.Sessions
+                .Select((s, ix) => {
+                    var name = $"{s.Ident}-{ix}.html";
+                    var url = Uri.EscapeDataString(name);
+                    var date = s.StartTime;
+                    var participants = s.Body
+                        .Select(l => l.Username)
+                        .Distinct()
+                        .OrderBy(u => !u.Equals("Crion", StringComparison.OrdinalIgnoreCase))
+                        .ThenBy(u => u);
+
+                    return $@"<a href=""{url}"">{name}</a>
+    <span>{date}</span>
+    <span>{string.Join(", ", participants)}</span>";});
+
+            return $@"{prefix}{stylesheet}<h2>{title}</h2>
+<div class=""session-grid"">
+    <span class=""th"">Session</span>
+    <span class=""th"">Date</span>
+    <span class=""th"">Participants</span>
+    {string.Join("\n\t", links)}
+</div>";
         }
     }
 }
