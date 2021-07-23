e721a8e9d98651d646ba55e9b3aca9b3fd9a85aa
Thomas Castiglione <castiglione@mac.com>
2021-07-24 00:21:45 +0800

    mark vignettes

--- Dispel/DispelOptions.cs
+++ Dispel/DispelOptions.cs
@@ -6,7 +6,8 @@ namespace Dispel
     {
         public string Title { get; set; }
         public string GM { get;set; }
-        public Dictionary<string, string> Aliases {get ; set; }
-        public Dictionary<string, int[]> Groups {get ; set; }
+        public Dictionary<string, string> Aliases { get; set; }
+        public Dictionary<string, int[]> Groups { get; set; }
+        public int[] Vignettes { get; set; }
     }
 }
