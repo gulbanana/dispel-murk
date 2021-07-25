a35236addf5e1edee54f3bfe95a888dd09ccebc0
Thomas Castiglione <castiglione@mac.com>
2021-07-25 14:39:24 +0800

    fix website build

--- Dispel.Web/Pages/Index.cshtml.cs
+++ Dispel.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
@@ -8,19 +9,27 @@ namespace Dispel.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly DispelOptions options;
+
         [BindProperty, Required, Display(Name = "Log File")]
         public IFormFile LogFile { get; set; }
 
         [BindProperty, Required]
         public string Format { get; set; }
 
+        public IndexModel(IOptions<DispelOptions> options)
+        {
+            this.options = options.Value;
+        }
+
         public async Task<ActionResult> OnPostAsync()
         {
+            var e = new Engine(options);
             var f = Formats.Parse(Format);
 
             using (var file = LogFile.OpenReadStream())
             {
-                await Engine.ConvertSingleAsync(file, Response.Body, f);
+                await e.ConvertSingleAsync(file, Response.Body, f);
             }
 
             return new EmptyResult();
